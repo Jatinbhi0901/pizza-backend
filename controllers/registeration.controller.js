9d20aed20c3012fa9a66ea07e333dc6f9f91b06a
Mansi Bhondave <mansibhondave@gmail.com>
2022-03-23 22:42:42 +0530

    hashed password

--- controllers/registeration.controller.js
+++ controllers/registeration.controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const register = require('../models/registeration');
+const bcrypt = require("bcrypt");
 
 exports.registerUser = (req,res, next) => {
     register.find({email: req.body.email})
@@ -11,27 +12,36 @@ exports.registerUser = (req,res, next) => {
             });
         }
         else{
-            const details  = new register({
-                email: req.body.email,
-                password: req.body.password
-            });
-            details
-            .save()
-            .then(result => {
-                if(result){
-                    res.status(200).json({
-                        message: 'User Registered successfully'
-                    })
-                }
-                else{
-                    res.status(400).json({
-                        message: 'Error while registering user'
-                    })
-                }
+            if (req.body.password !== req.body.confirmPassword) {
+                return res.status(400).json({
+                    message: "Password mismatch"
+                })
+
+            }
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                const details  = new register({
+                    email: req.body.email,
+                    password: hash
+                });
+                details
+                .save()
+                .then(result => {
+                    if(result){
+                        res.status(200).json({
+                            message: 'User Registered successfully'
+                        })
+                    }
+                    else{
+                        res.status(400).json({
+                            message: 'Error while registering user'
+                        })
+                    }
+                })
+                .catch(err => {
+                    res.send(err)
+                })
             })
-            .catch(err => {
-                res.send(err)
-            })
+            
         }
     })
     .catch(err => {
--- models/registeration.js
+++ models/registeration.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const registerationSchema = new mongoose.Schema({
     email: {
-        type: String,
-        required: true
+        type: String, unique: true,required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is invalid")
+            }
+        }
     },
     password: {type: String, required: true}
 })
