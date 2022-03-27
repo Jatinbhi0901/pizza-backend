1ccc01ae13b07ca41ffb131803d93f8c1203084d
Mansi Bhondave <mansibhondave@gmail.com>
2022-03-27 16:46:50 +0530

    update profile

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -15,3 +15,45 @@ exports.getProfile = (req,res,next) => {
         })
     })
 }
+
+exports.updateProfile = (req,res,next) => {
+    let firstName,lastName,phnNo, dateOfBirth, gender, country, state, address;
+    firstName= req.body.firstName,
+    lastName= req.body.lastName,
+   // email: req.body.email,
+    phnNo= req.body.phnNo,
+    dateOfBirth= req.body.dateOfBirth,
+    gender= req.body.gender,
+    country= req.body.country,
+    state= req.body.state,
+    address= req.body.address
+
+    profile.findOneAndUpdate({email: req.body.email},{
+        $set: {
+            firstName,
+            lastName,
+            phnNo,
+            dateOfBirth,
+            gender,
+            country,
+            state,
+            address
+        }
+    })
+    .exec()
+    .then(result => {
+        if(!result){
+            return res.status(401).json({
+                message: 'User not found'
+            })
+        }
+        return res.status(200).json({
+            message: 'Profile updated successfully'
+        })
+    })
+    .catch(err => {
+        res.status(400).json({
+            message: err
+        })
+    })
+}
--- routes/profile.route.js
+++ routes/profile.route.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose');
 const profileController = require('../controllers/profile.controller');
 
 router.get('/getProfile', profileController.getProfile);
+router.patch('/updateProfile', profileController.updateProfile);
 
 module.exports = router;
